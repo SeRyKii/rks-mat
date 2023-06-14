29b2c1b88082e50b57b9d8ffaa3900f7459b016e
SeRyKii <kop-woj@wp.pl>
2023-06-14 12:11:46 +0200

    fix: Better animations

--- src/lib/Trainer.svelte
+++ src/lib/Trainer.svelte
@@ -122,14 +122,14 @@
 >
 	{#if !active}
 		<header
-			in:fly={{ x: -2000, duration: 500 }}
+			in:fly={{ y: 2000, duration: 500 }}
 			out:fade
 			class="card-header overflow-hidden aspect-[21/12] -m-4 mb-0"
 		>
 			<img class="w-full" src={image} alt={name} />
 		</header>
 		<hr />
-		<section in:fly={{ x: -2000, duration: 500 }} out:fade class="p-4 flex flex-col">
+		<section in:fly={{ y: 2000, duration: 500 }} out:fade class="p-4 flex flex-col">
 			<span class="text-3xl font-bold whitespace-nowrap">{name}</span>
 			<span class="text-justify">
 				{description}
@@ -137,7 +137,8 @@
 		</section>
 	{:else}
 		<div
-			in:fly={{ x: -2000, duration: 500 }}
+			in:fly={{ y: -2000, duration: 500 }}
+			out:fade
 			class="top-0 absolute w-full items-center logo-cloud grid-cols-1 xl:grid-cols-2 2xl:grid-cols-4 gap-0.5"
 		>
 			<!-- svelte-ignore a11y-missing-attribute -->
